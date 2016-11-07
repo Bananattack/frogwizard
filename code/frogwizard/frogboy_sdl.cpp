6f910deac338c6f3dfbc2fffd9ea4c02e19b159f
Andrew G. Crowell <overkill9999@gmail.com>
2016-11-06 19:05:56 -0500

    For SDL target, use their renderer instead of accessing the window surface directly. Add ability to resize the window. Snap the window to the nearest integer ratio to pretty ugly jagged resizes. This takes care of most of the work to scale the screen to arbitrary sizes. Next up will probably be figuring out some way to lower CPU usage without sacrificing too much framerate.

--- code/frogwizard/frogboy_sdl.cpp
+++ code/frogwizard/frogboy_sdl.cpp
@@ -9,12 +9,15 @@
 namespace {
     bool open = false;
     SDL_Window* window = nullptr;
-    SDL_Surface* windowSurface = nullptr;
-    SDL_Surface* backSurface = nullptr;
+    SDL_Renderer* renderer = nullptr;
+    SDL_Texture* screenTexture = nullptr;
+    SDL_Surface* screenSurface = nullptr;
     uint8_t screenBuffer[frogboy::SCREEN_WIDTH * (frogboy::SCREEN_HEIGHT / 8)];
     uint32_t lastFrame = 0;
 
     const uint8_t SCREEN_SCALE = 8;
+
+    bool windowMaximized = false;
 
     bool pressed[static_cast<size_t>(frogboy::BUTTON_COUNT)];
 
@@ -33,19 +36,25 @@ namespace frogboy {
         srand(static_cast<unsigned int>(time(nullptr)));
 
         SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
-        window = SDL_CreateWindow(FROGBOY_APPNAME, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH * SCREEN_SCALE, SCREEN_HEIGHT * SCREEN_SCALE, 0);
-        windowSurface = SDL_GetWindowSurface(window);
-        backSurface = SDL_CreateRGBSurface(0, SCREEN_WIDTH, SCREEN_HEIGHT, 32, 0, 0, 0, 0);
+        window = SDL_CreateWindow(FROGBOY_APPNAME, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH * SCREEN_SCALE, SCREEN_HEIGHT * SCREEN_SCALE, SDL_WINDOW_RESIZABLE);
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+
+        SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
+        SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+        screenTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
+        screenSurface = SDL_CreateRGBSurface(0, SCREEN_WIDTH, SCREEN_HEIGHT, 32, 0, 0, 0, 0);
         clearScreen();
         lastFrame = SDL_GetTicks();
 
         memset(pressed, 0, sizeof(pressed));
 
+        windowMaximized = false;
         open = true;
     }
 
     void destroy() {
-        SDL_FreeSurface(backSurface);
+        SDL_FreeSurface(screenSurface);
+        SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
         SDL_Quit();
     }
@@ -55,10 +64,10 @@ namespace frogboy {
     }
 
     void refreshScreen() {
-        SDL_LockSurface(backSurface);
-        uint32_t black = SDL_MapRGBA(backSurface->format, 0, 0, 0, 0);
-        uint32_t white = SDL_MapRGBA(backSurface->format, 255, 255, 255, 0);
-        uint32_t* surfaceRowPtr = static_cast<uint32_t*>(backSurface->pixels);
+        SDL_LockSurface(screenSurface);
+        uint32_t black = SDL_MapRGBA(screenSurface->format, 0, 0, 0, 0);
+        uint32_t white = SDL_MapRGBA(screenSurface->format, 255, 255, 255, 0);
+        uint32_t* surfaceRowPtr = static_cast<uint32_t*>(screenSurface->pixels);
         for(size_t y = 0; y != SCREEN_HEIGHT; ++y) {
             uint32_t* ptr = surfaceRowPtr;
             for(size_t x = 0; x != SCREEN_WIDTH; ++x) {
@@ -68,20 +77,15 @@ namespace frogboy {
                 ptr++;
             }
             surfaceRowPtr = static_cast<uint32_t*>(static_cast<void*>(
-                    static_cast<uint8_t*>(static_cast<void*>(surfaceRowPtr)) + backSurface->pitch));
+                    static_cast<uint8_t*>(static_cast<void*>(surfaceRowPtr)) + screenSurface->pitch));
         }
-        SDL_UnlockSurface(backSurface);
+        SDL_UnlockSurface(screenSurface);
 
-        SDL_FillRect(windowSurface, NULL, black);
-
-        SDL_Rect destRect;
-        destRect.x = 0;
-        destRect.y = 0;
-        destRect.w = SCREEN_WIDTH * SCREEN_SCALE;
-        destRect.h = SCREEN_HEIGHT * SCREEN_SCALE;        
-        SDL_BlitScaled(backSurface, nullptr, windowSurface, &destRect);
-
-        SDL_UpdateWindowSurface(window);
+        SDL_UpdateTexture(screenTexture, NULL, screenSurface->pixels, screenSurface->pitch);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, screenTexture, NULL, NULL);
+        SDL_RenderPresent(renderer);
     }
 
     bool waitForFrame() {
@@ -89,28 +93,67 @@ namespace frogboy {
             return false;
         }
 
-        SDL_Event e;
-        while(SDL_PollEvent(&e) != 0) {
-            switch(e.type) {
-                case SDL_QUIT:
-                    open = false;
-                    break;
-                case SDL_KEYDOWN:
-                    for(size_t i = 0; i != BUTTON_COUNT; ++i) {
-                        if(e.key.keysym.sym == keycodes[i]) {
-                            pressed[i] = true;
-                            break;
+        bool resized = false;
+
+        {
+            SDL_Event e;
+            while(SDL_PollEvent(&e) != 0) {
+                switch(e.type) {
+                    case SDL_QUIT:
+                        open = false;
+                        break;
+                    case SDL_KEYDOWN:
+                        for(size_t i = 0; i != BUTTON_COUNT; ++i) {
+                            if(e.key.keysym.sym == keycodes[i]) {
+                                pressed[i] = true;
+                                break;
+                            }
                         }
-                    }
-                    break;
-                case SDL_KEYUP:
-                    for(size_t i = 0; i != BUTTON_COUNT; ++i) {
-                        if(e.key.keysym.sym == keycodes[i]) {
-                            pressed[i] = false;
-                            break;
+                        break;
+                    case SDL_KEYUP:
+                        for(size_t i = 0; i != BUTTON_COUNT; ++i) {
+                            if(e.key.keysym.sym == keycodes[i]) {
+                                pressed[i] = false;
+                                break;
+                            }
                         }
-                    }
-                    break;
+                        break;
+                    case SDL_WINDOWEVENT:
+                        switch(e.window.event) {
+                            case SDL_WINDOWEVENT_RESIZED: {
+                                resized = true;
+                                break;
+                            }
+                            case SDL_WINDOWEVENT_MAXIMIZED: {
+                                windowMaximized = true;
+                                break;
+                            }
+                            case SDL_WINDOWEVENT_RESTORED: {
+                                windowMaximized = false;
+                                break;
+                            }
+                        }
+                }
+            }
+        }
+
+        if(!windowMaximized && resized) {
+            int width, height;
+            SDL_GetWindowSize(window, &width, &height);
+            int xscale = (width + SCREEN_WIDTH / 2) / SCREEN_WIDTH;
+            int yscale = (height + SCREEN_WIDTH / 2) / SCREEN_WIDTH;
+
+            if(width % SCREEN_WIDTH != 0
+            || height % SCREEN_HEIGHT != 0
+            || xscale != yscale) {
+                int scale = xscale;
+                if(scale < yscale) {
+                    scale = yscale;
+                }
+                if(scale < 1) {
+                    scale = 1;
+                }
+                SDL_SetWindowSize(window, scale * SCREEN_WIDTH, scale * SCREEN_HEIGHT);
             }
         }
 
